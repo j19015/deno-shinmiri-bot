2f9103f7b65f33924e33fa891d45c4dde088e0d6
j19015 <j19015@sangi.jp>
2023-01-31 09:16:55 +0900

    change how to give webhook_url

--- main.ts
+++ main.ts
@@ -1,2 +1,2 @@
 import discord from "./discord.ts";
-await discord("webhookURLが入ります","しんみりしてきましたねぇ","Pancake","https://free-materials.com/adm/wp-content/uploads/2019/12/adpDSC_2348-1024x683.jpg");
+await discord(Deno.env.get('WEBHOOK_URL'),"しんみりしてきましたねぇ","Pancake","https://free-materials.com/adm/wp-content/uploads/2019/12/adpDSC_2348-1024x683.jpg");
